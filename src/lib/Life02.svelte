fb37c9df38b99abe11c9e2d486afd6e1ca220e7b
JIN_DDIT <zanalyst01@gmail.com>
2025-05-28 18:20:07 +0900

    feat: svelte lifecycle

--- src/lib/Life02.svelte
+++ src/lib/Life02.svelte
@@ -1,13 +1,13 @@
 <script>
   import {onDestroy, onMount} from "svelte";
 
-  let msg = "LIFE01"
+  let msg = "LIFE02"
 
   onMount(()=>{
-      console.log("LIFE01 onMount")
+      console.log("LIFE02 onMount")
   })
   onDestroy(()=>{
-      console.log("LIFE01 onDestroy")
+      console.log("LIFE02 onDestroy")
   })
 
 </script>
